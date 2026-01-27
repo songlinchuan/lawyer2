b68ba5c623ae2dee27bce73cfd88f27a2a685b38
songlinchuan <songlinchuan0098@gmail.com>
2026-01-27 17:49:26 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,10 +14,10 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: '宋临川律师 | 民商事与刑事法律服务',
-  description: '诚心诚意，尽心尽力。 中华人民共和国执业律师。', // 注意：我在句号后面加了一个空格
+  description: '诚心诚意，尽心尽力。　　中华人民共和国执业律师。', 
   openGraph: {
     title: '宋临川律师 | 民商事与刑事法律服务',
-    description: '诚心诚意，尽心尽力。 中华人民共和国执业律师。', // 这里也加了空格
+    description: '诚心诚意，尽心尽力。　　中华人民共和国执业律师。',
     url: 'https://www.songlinchuan.com',
     siteName: '宋临川律师个人官网',
     images: [
